64e2a70695a08d938ceccdecd943643e7d1941dc
mani333007 <mani333007@gmail.com>
2022-04-13 12:25:12 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <iframe
+        src="https://my.matterport.com/show/?m=QKXyLp3Kd6k"
+        frameBorder="0"
+        allowFullScreen
+        allow="xr-spatial-tracking"
+        style={{ width: '100vw', height: '100vh' }}
+      />
   );
 }
 
